040d8cb226fd4bdf4a34f45b074a4f2d6534be3b
pablopedernera0 <64797424+pablopedernera0@users.noreply.github.com>
2026-05-08 11:10:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import mysql.connector
 app = Flask(__name__)
 
 DB_CONFIG = {
-    "host": "127.0.0.1",
+    "host": "172.18.0.2",
     "port": 3306,
     "user": "root",
     "password": "mysecretpassword",
